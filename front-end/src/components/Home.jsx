1348072226df04c539607373d370ad7d61999919
thatallatom <atomdota24@gmail.com>
2023-10-04 19:57:22 +0700

    Summary

---
+++ front-end/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import Nav from "./Nav";
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <div>
+        <h3>Home</h3>
+        <p>This is home pages</p>
+      </div>
+    </>
+  );
+}
